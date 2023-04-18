7ee93d354ae495efece7e630c2b93a05b42fb276
Misael Guerrero <mguerrerowest@gmail.com>
2023-04-18 13:26:55 -0400

    create button event listener to search for new city and console log its data

--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
 import getWeatherData from "./modules/getData"
 
-const weatherInfo = getWeatherData('chicago')
+let currentCity = 'New York'
+
+const searchButton = document.querySelector('.submitCity')
+const city = document.querySelector('.searchCity')
+
+const weatherInfo = getWeatherData(currentCity)
 
 weatherInfo.then(function(data){ 
     const info = data
     console.log(info)
 })
+
+searchButton.addEventListener('click', () => {
+    currentCity = city.value
+    console.log(currentCity)
+    getWeatherData(currentCity)
+
+    const weatherInfo = getWeatherData(currentCity)
+
+    weatherInfo.then(function(data){ 
+        const info = data
+        console.log(info)
+    })
+    city.value = ''
+})
+
+
