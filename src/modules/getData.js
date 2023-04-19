5893cb76b6055aced4b36151cac8b003c51f23ea
Misael Guerrero <mguerrerowest@gmail.com>
2023-04-19 11:31:30 -0400

    handle error with an alert

--- src/modules/getData.js
+++ src/modules/getData.js
@@ -1,13 +1,17 @@
 async function getWeatherData(city){
-    let response = await fetch(`https://api.weatherapi.com/v1/current.json?key=7b0154a5196c4135a74133618231404&q=${city}`, {mode: 'cors'})
-    let weatherData = await response.json();
-    let cityName = weatherData.location.name
-    let country = weatherData.location.country
-    let condition = weatherData.current.condition.text
-    let tempF = weatherData.current.temp_f
-    let tempC = weatherData.current.temp_c
-    let info = [cityName, country, condition, tempC, tempF]
-    return info
+    try{
+        let response = await fetch(`https://api.weatherapi.com/v1/current.json?key=7b0154a5196c4135a74133618231404&q=${city}`, {mode: 'cors'})
+        let weatherData = await response.json();
+        let cityName = weatherData.location.name
+        let country = weatherData.location.country
+        let condition = weatherData.current.condition.text
+        //let tempF = weatherData.current.temp_f
+        let tempC = weatherData.current.temp_c
+        let info = [cityName, country, condition, tempC /*tempF*/]
+        return info
+    } catch (error) {
+        alert(error)
+    }
 }
 
 export default getWeatherData
